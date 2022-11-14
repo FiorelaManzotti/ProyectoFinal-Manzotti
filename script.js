6212c4d053c13c1b179e7d29193a0e6e5d22459b
Fiorela Manzotti <fiomanzotti@hotmail.com>
2022-11-14 14:48:18 -0300

    guardando estilos en el tp

--- script.js
+++ script.js
@@ -118,37 +118,39 @@ function load (){
 
 //INICIO WEATHER CHANNEL
 
-function cargarCiudad(ciudad) {
-    $.getJSON(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=95176c8edea30e33338e0eaddd53a916&units=metric&lang=es`, function(data){
-        document.querySelector("#ciudad").textContent = data.name
-        document.querySelector("#temperatura").textContent = (Math.round((data.main.temp)*10))/10
-        document.querySelector("#temperatura").innerHTML += "<sup>°C</sup>"
+//usando AJAX y JSON
+//Funcion ingresar ciudad para ver el clima
+function enterCity(city) {
+    $.getJSON(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=95176c8edea30e33338e0eaddd53a916&units=metric&lang=es`, function(data){
+        document.querySelector("#city").textContent = data.name
+        document.querySelector("#temperature").textContent = (Math.round((data.main.temp)*10))/10
+        document.querySelector("#temperature").innerHTML += "<sup>°C</sup>"
         document.querySelector("#wicon").src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-        document.querySelector("#descripcion").textContent = data.weather[0].description
-        document.querySelector(".container").style.visibility = "visible"
+        document.querySelector("#description").textContent = data.weather[0].description
+        document.querySelector(".containerWeather").style.visibility = "visible"
         console.dir(data)
     })
     .fail(function() {
-        alert("Ciudad no encontrada");
+        alert("City not found");
     })
 }
-document.querySelector("button").addEventListener("click", function () {
-    if (!document.querySelector("input").value) {
-        alert("Debe ingresar una ciudad") 
+document.querySelector("#buttonWeather").addEventListener("click", function () {
+    if (!document.querySelector("#inputWeather").value) {
+        alert("Must enter a city") 
     } else {
-        let ciudad = document.querySelector("input").value.split(" ").join("%20")
-        document.querySelector("input").value = ""
-        cargarCiudad(ciudad)
+        let city = document.querySelector("#inputWeather").value.split(" ").join("%20")
+        document.querySelector("#inputWeather").value = ""
+        enterCity(city)
     }
 })
 document.body.addEventListener("keydown", function (info) {
     if (info.key === "Enter") {
-        if (!document.querySelector("input").value) {
-            alert("Debe ingresar una ciudad") 
+        if (!document.querySelector("inputWeather").value) {
+            alert("Must enter a city") 
         } else {
-            let ciudad = document.querySelector("input").value.split(" ").join("%20")
-            document.querySelector("input").value = ""
-            cargarCiudad(ciudad)
+            let city = document.querySelector("inputWeather").value.split(" ").join("%20")
+            document.querySelector("inputWeather").value = ""
+            enterCity(city)
         }
     }
 })
@@ -174,7 +176,7 @@ function callbackClick () {
 const hoverBoton = document.getElementById ("botonEnviar");
 
 hoverBoton.addEventListener("mouseover", () => {
-    hoverBoton.style.backgroundColor = "red"
+    hoverBoton.style.backgroundColor = "darkgreen"
 })
 
 hoverBoton.addEventListener("mouseout", () => {
